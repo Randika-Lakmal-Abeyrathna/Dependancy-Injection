ea863c6044589f3b769a71a03bdaf5829603b81b
Randika Lakmal <randikalakmalabeyrathna93@gmail.com>
2018-01-21 21:04:02 +0530

    Init Project

---
+++ src/main/java/lk/randika/DipendancyInjectionApplication.java
@@ -0,0 +1,12 @@
+package lk.randika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DipendancyInjectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DipendancyInjectionApplication.class, args);
+	}
+}
